c70eb47aec8915639ae32569b5549f93e731ee1a
your Name <mohamedabdulayubm@gmail.com>
2022-06-21 13:54:50 +0530

    line

--- Day_8 Line/Program.cs
+++ Day_8 Line/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+ //A Length as 2 Points(x1, y1) and(x2, y2) 
+ // - Length of a Line = sqrt( (x2 - x1) ^2 + (y2- y1) ^ 2)
+
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+double Length = ((Math.Pow(x2, 2)) - (Math.Pow(x1, 2))) + ((Math.Pow(y2, 2)) - (Math.Pow(y1, 2)));
+Console.WriteLine(Math.Sqrt(Length));
